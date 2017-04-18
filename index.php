3b3b0d1a589d0a31c75ab788d49cc073566ae6aa
joshsole <Josh@Joshs-MacBook-Pro.local>
2017-04-19 10:48:07 +1200

    code tidy

--- index.php
+++ index.php
@@ -5,24 +5,24 @@ require 'includes/config.php';
 
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    
-    if ($_POST["_method"] == "delete") {
-        $id=e($_POST['id']);
-        deleteProject($id, $dbh);
-        redirect('index.php');
-    }
 
-    if ($_POST["_method"] == "edit") {
-        $id=e($_POST['editid']);
-            // editProject($id, $dbh);
-        redirect('edit.php?id=' . $id);
-    }
+  if ($_POST["_method"] == "delete") {
+    $id=e($_POST['id']);
+    deleteProject($id, $dbh);
+    redirect('index.php');
+  }
 
-    if ($_POST["_method"] == "view") {
-        $id=$_POST['viewid'];
+  if ($_POST["_method"] == "edit") {
+    $id=e($_POST['editid']);
             // editProject($id, $dbh);
-        redirect('view.php?id=' . $id);
-    }
+    redirect('edit.php?id=' . $id);
+  }
+
+  if ($_POST["_method"] == "view") {
+    $id=$_POST['viewid'];
+            // editProject($id, $dbh);
+    redirect('view.php?id=' . $id);
+  }
 }
 
 require 'partials/header.php';
@@ -34,77 +34,63 @@ require 'partials/navigation.php';
 
 <!-- Start of Content -->
 <div class="container">
-    <div class="row">
-        <div class="col-md-12"><?= showMessages() ?>
-        </div>
+  
+  <div class="row">
+    <div class="col-md-12"><?= showMessages() ?>
     </div>
+  </div>
 
-    <div class="row">
+  <div class="row">
+    <?php foreach ($projects as $project):?>
 
-        <?php foreach ($projects as $project):?>
+     <!-- Start of Card -->
+     <div class="col-md-3">
+      <div class="panel panel-default">
+        <div class="panel-heading card-header">
+          <img class="img-responsive" src="<?= $project['url'] ?>">
+        </div>
 
-           <!-- Start of Card -->
-           <div class="col-md-3">
-            <div class="panel panel-default">
-                <div class="panel-heading card-header">
-                    <img class="img-responsive" src="<?= $project['url'] ?>">
-                </div>
+        <div class="panel-body">
+          <h4><?= substr ($project['title'], 0, 20) ?></h4>
+          <p><?= substr ($project['content'], 0, 80) ?></p>
+          <form method="POST" action="index.php" style="display: inline-block;">
+            <input name="_method" type="hidden" value="view">
+            <input name="viewid" type="hidden" value="<?= $project['id'] ?>">
+            <button type="submit" class="btn btn-default btn-xs">
+              <i class="icon ion-eye">
+              </i> View
+            </button>
+          </form>
 
-                <div class="panel-body">
-                    <h4><?= substr ($project['title'], 0, 20) ?></h4>
-                    <p><?= substr ($project['content'], 0, 80) ?></p>
-                    
+          <div class="pull-right">
+            <?php if(userOwns($project['user_id'])): ?>
+              <!-- Edit Button and Form -->
+              <form method="POST" action="index.php" style="display: inline-block;">
+                <input name="_method" type="hidden" value="edit">
+                <input name="editid" type="hidden" value="<?= $project['id'] ?>">
+                <button type="submit" class="btn btn-info btn-xs">
+                 <i class="icon ion-edit">
+                 </i> Edit
+               </button>
+             </form>
+             <!-- Delete Button and Form -->
+             <form method="POST" action="index.php" style="display: inline-block;">
+               <input name="_method" type="hidden" value="delete">
+               <input name="id" type="hidden" value="<?= $project['id'] ?>">
+               <button onclick="return confirm('Are you sure you want to delete this item?');" type="submit" class="btn btn-default btn-xs btn-danger">
+                 <i class="icon ion-ios-close-outline">
+                 </i> Delete
+               </button>
+             </form>
+           <?php endif; ?>
 
-                    <form method="POST" action="index.php" style="display: inline-block;">
-
-                      <input name="_method" type="hidden" value="view">
-
-                      <input name="viewid" type="hidden" value="<?= $project['id'] ?>">
-
-                      <button type="submit" class="btn btn-default btn-xs">
-                        <i class="icon ion-eye"></i> View
-                    </button>
-                </form>
-
-
-                <div class="pull-right">
-                <?php if(userOwns($project['user_id'])): ?>
-                    <!-- Edit Button and Form -->
-                    <form method="POST" action="index.php" style="display: inline-block;">
-
-                     <input name="_method" type="hidden" value="edit">
-
-                     <input name="editid" type="hidden" value="<?= $project['id'] ?>">
-
-                     <button type="submit" class="btn btn-info btn-xs">
-                       <i class="icon ion-edit"></i> Edit
-                   </button>
-               </form>
-
-               <!-- Delete Button and Form -->
-               <form method="POST" action="index.php" style="display: inline-block;">
-
-                   <input name="_method" type="hidden" value="delete">
-
-                   <input name="id" type="hidden" value="<?= $project['id'] ?>">
-                   <button onclick="return confirm('Are you sure you want to delete this item?');" type="submit" class="btn btn-default btn-xs btn-danger">
-                       <i class="icon ion-ios-close-outline"></i> Delete
-                   </button>
-
-               </form>
-               <?php endif; ?>
-
-           </div>
-
+         </div>
        </div>
-
+     </div>
    </div>
-</div>
-<?php endforeach; ?>
-
+ <?php endforeach; ?>
 </div>
 </div>
-
 
 <!-- End of Card -->
 
